746704cd35b070170e515952c182f6a1613d581f
Bob <bkane90@gmail.com>
2020-12-03 09:15:33 -0500

    working on it

--- index.js
+++ index.js
@@ -17,11 +17,32 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+    // return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({})
+    Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: 'Grilled Cheese',
+      level: 'UltraPro Chef',
+      ingredients: [
+        'bread',
+        'butter',
+        'cheese'
+      ],
+      cuisine: 'American',
+      dishType: 'main_course',
+      image: 'https://www.tasteofhome.com/recipes/super-grilled-cheese-sandwiches/',
+      duration: 5,
+      creator: 'Bobby Slay'
+    })
+      .then(recipe => console.log(recipe.title))
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+Recipe.insertMany(data0.then((recipes) => {
+  console.log(recipes);
+});
+
+  .catch (error => {
+  console.error('Error connecting to the database', error);
+});
